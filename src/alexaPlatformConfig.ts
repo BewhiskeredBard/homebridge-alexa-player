30d01b1848c7509979adfe2dedb9ce85e13c6c8a
Bewhiskered Bard <112001+BewhiskeredBard@users.noreply.github.com>
2022-05-22 21:50:41 -0700

    Fix formatting issue from previous pull request. (#486)

--- src/alexaPlatformConfig.ts
+++ src/alexaPlatformConfig.ts
@@ -2,7 +2,16 @@ import type { PlatformConfig } from 'homebridge';
 
 export const PLATFORM_NAME = 'AlexaPlayer';
 
-export type AmazonDomain = 'amazon.com' | 'amazon.ca' | 'amazon.de' | 'amazon.es' | 'amazon.fr' | 'amazon.it' | 'amazon.co.jp' | 'amazon.co.uk' | 'amazon.com.mx';
+export type AmazonDomain =
+    | 'amazon.com'
+    | 'amazon.ca'
+    | 'amazon.de'
+    | 'amazon.es'
+    | 'amazon.fr'
+    | 'amazon.it'
+    | 'amazon.co.jp'
+    | 'amazon.co.uk'
+    | 'amazon.com.mx';
 
 export interface AlexaPlatformConfig extends PlatformConfig {
     platform: 'AlexaPlayer';
